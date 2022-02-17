b52505efd7056c155933e9ddd8ee026385fc5a5e
ALAHLYMOMKN\nour.hussein <nour.hussein@momkn.com.eg>
2022-02-17 17:41:10 +0200

    empty commit

--- Power.Core/Services/Implemenation/AuthService.cs
+++ Power.Core/Services/Implemenation/AuthService.cs
@@ -37,7 +37,7 @@ namespace Power.Core.Services.Implemenation
                 return "Invalid user or role";
 
             if (await _userManager.IsInRoleAsync(user, dto.RoleName))
-                return " User is already assigned to this role from develop branch by merged command !";
+                return " User is already assigned to this role !";
 
             var result = await _userManager.AddToRoleAsync(user, dto.RoleName);
 
